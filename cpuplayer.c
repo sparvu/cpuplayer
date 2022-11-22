12f191be7f804d78aeb3fbe04988023faacd712f
Stefan Parvu <sparvu@kronometrix.org>
2022-11-22 16:37:04 +0200

    small fixes for copyright changes

--- cpuplayer.c
+++ cpuplayer.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2014 PerfDynamics. All rights reserved.
- * Copyright (c) 2014 Stefan Parvu (www.systemdatarecorder.org)
+ * Copyright (c) 2022 Stefan Parvu. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
